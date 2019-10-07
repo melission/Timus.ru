bb264f90326941cf0738915ab8f6363437b77f0b
melission <melission@users.noreply.github.com>
2019-10-07 12:46:05 +0300

    some changes

---
+++ 1243.py
@@ -0,0 +1,5 @@
+# http://acm.timus.ru/problem.aspx?space=1&num=1243&locale=en
+
+devorsed_items = int(input().strip())
+snowhite = devorsed_items % 7
+print(snowhite)
--- 1493.py
+++ 1493.py
@@ -2,9 +2,11 @@
 import sys
 frst_half = []
 scnd_half = []
-next_ticket_num = 0
+frst_part = 0
+scnd_part = 0
 for str_num in sys.stdin:
-    next_ticket_num = str_num[3:].strip()
+    frst_part = str_num[3:].strip()
+    scnd_part = str_num[3:].strip()
     for item in str_num[:3].strip():
         item = int(item)
         frst_half.append(item)
